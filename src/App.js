9eeb4f7f48828ed8913c22b348f9ad58ad994516
AliveKiwi <prateekjais.dev@gmail.com>
2024-02-17 04:18:34 +0530

    3. Data Passing: Passed a function from parent component to child component so that child can send data to parent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import SearchBar from './components/SearchBar';
 function App() {
-  return <div>App</div>;
+  const handleSubmit = (term) => {
+    console.log('Do a search with', term);
+  };
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
+
+// Child To Parent Communication
+// 1. Import the Child Component in Parent
+// 2. Define a function in parent
+// 3. Pass it down as prop to Child Component
+// 4. Use the function passed as prop in child component
