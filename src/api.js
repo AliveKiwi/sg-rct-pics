36966cc3fce6d0f7eb348dfaa91a69a6fddcc2c7
AliveKiwi <prateekjais.dev@gmail.com>
2024-02-16 09:16:06 +0530

    2. axios: Made SearchImage Dynamic and removed function call from App

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const searchImage = async () => {
+const searchImage = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
       Authorization: `Client-ID 1OLothho6Nr77AKRRCSPvBoMMzNPKH5p5rDHivEItqY`,
     },
-    params: { query: 'cars' },
+    params: { query: term },
   });
-  console.log(response);
-  return response;
+
+  return response.data.results;
 };
 
 export default searchImage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import searchImage from './api';
-
-searchImage();
 
 const el = document.getElementById('root');
 
